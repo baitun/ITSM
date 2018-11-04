01e923f4e02128bef5ca69b3991eb7a5465b5d40
Yury Savin <savinyurii@gmail.com>
2018-11-05 04:42:27 +0800

    Use LocaleProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import TableTickets from './TableTickets';
+import { LocaleProvider } from 'antd';
+import ru_RU from 'antd/lib/locale-provider/ru_RU';
 
 class App extends React.Component {
   render() {
     return (
-      <TableTickets />
+      <LocaleProvider locale={ru_RU}>
+        <TableTickets />
+      </LocaleProvider>
     );
   }
 }
