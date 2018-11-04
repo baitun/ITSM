c3d7ead6e7086c8774a14580211eba03f9beb3fd
Yury Savin <savinyurii@gmail.com>
2018-11-05 01:22:29 +0800

    itin Ant Design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Table from 'antd/lib/table';
 
 class App extends React.Component {
   render() {
     return (
-      <div>test</div>
+      <Table />
     );
   }
 }
