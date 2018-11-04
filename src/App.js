d119e40d1742f40584c36095f80105c6b81f8b08
Yury Savin <savinyurii@gmail.com>
2018-11-05 06:15:52 +0800

    Basic Route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import TableTickets from './TableTickets';
 import { LocaleProvider } from 'antd';
 import ru_RU from 'antd/lib/locale-provider/ru_RU';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <LocaleProvider locale={ru_RU}>
-        <TableTickets />
+        <Router>
+          <React.Fragment>
+            <Link to='/ITSM' >Table tickets</Link>
+            <Switch>
+              {/* <Redirect from='/' to='/ITSM/' /> */}
+              <Route path="/" exact component={TableTickets} />
+              <Route path="/ITSM" exact component={TableTickets} />
+              <Route component={() => <div>404</div>} />
+            </Switch>
+          </React.Fragment>
+        </Router>
       </LocaleProvider>
     );
   }
