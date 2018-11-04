6e7c8b6f49e8d201f72ec53b5bb4359005ec8957
Yury Savin <savinyurii@gmail.com>
2018-11-05 04:13:04 +0800

    Sort and filter TableTickets

--- src/TableTickets.js
+++ src/TableTickets.js
@@ -2,19 +2,66 @@ import React from 'react'
 import { Table } from 'antd';
 
 const columns = [
-  { dataIndex: 'locality', title: 'Locality (City)' },
-  { dataIndex: 'system', title: 'System name' },
-  { dataIndex: 'Description', title: 'Description (comment)' },
-  { dataIndex: 'entryid', title: 'BPM Ticket' },
-  { dataIndex: 'status', title: 'Status' },
-  { dataIndex: 'bdate', title: 'Date start' },
-  { dataIndex: 'edate', title: 'Date end' },
+  {
+    dataIndex: 'locality',
+    title: 'Locality (City)',
+    // filtered: true,
+    sorter: (a, b) => a.locality > b.locality,
+    onFilter: (value, record) => record.locality.indexOf(value) === 0,
+    filters: [
+      { text: 'Irkutsk', value: 'Irkutsk' },
+      { text: 'Rostov', value: 'Rostov' },
+      { text: 'Saransk', value: 'Saransk' },
+      { text: 'Chelyabinsk', value: 'Chelyabinsk' },
+    ],
+  },
+  {
+    dataIndex: 'system',
+    title: 'System name',
+    sorter: (a, b) => a.system > b.system
+  },
+  {
+    dataIndex: 'entryid',
+    title: 'BPM Ticket',
+    render: entryid => <a href={`https://bpmonline.com/?tt=${entryid}`} target='_blank' rel='noreferrer noopener'>{entryid}</a>,
+  },
+  {
+    dataIndex: 'status',
+    title: 'Status'
+  },
+  // {
+  //   dataIndex: 'bdate',
+  //   title: 'Date start'
+  // },
+  // {
+  //   dataIndex: 'edate',
+  //   title: 'Date end'
+  // },
 ];
 
+const tickets = require('./data/tickets.json');
+
+const TicketDetails = (record) => {
+  return (
+    <div>
+      {record.description}
+    </div>
+  );
+}
+
 class TableTickets extends React.Component {
+  handleTableChange = (pagination, filters, sorter) => {
+    console.log(sorter)
+  }
   render() {
     return (
-      <Table columns={columns} />
+      <Table
+        columns={columns}
+        dataSource={tickets}
+        rowKey='id'
+        onChange={this.handleTableChange}
+        expandedRowRender={TicketDetails}
+      />
     );
   }
 }
