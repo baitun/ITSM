6b71cc3c48245478d0518e76eb605f5ed2182742
Yury Savin <savinyurii@gmail.com>
2018-11-05 02:11:45 +0800

    Add columns

--- src/TableTickets.js
+++ src/TableTickets.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Table } from 'antd';
 
+const columns = [
+  { dataIndex: 'locality', title: 'Locality (City)' },
+  { dataIndex: 'system', title: 'System name' },
+  { dataIndex: 'Description', title: 'Description (comment)' },
+  { dataIndex: 'entryid', title: 'BPM Ticket' },
+  { dataIndex: 'status', title: 'Status' },
+  { dataIndex: 'bdate', title: 'Date start' },
+  { dataIndex: 'edate', title: 'Date end' },
+];
+
 class TableTickets extends React.Component {
   render() {
     return (
-      <Table />
+      <Table columns={columns} />
     );
   }
 }
