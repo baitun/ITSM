de673274137428616a509f3461ca3c7059d9b4f0
Yury Savin <savinyurii@gmail.com>
2018-11-05 02:20:25 +0800

    Updates

---
+++ precache-manifest.5b921d15ed4449534fc003159aade74f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "019f9177a64b96354531",
+    "url": "/itsm/static/js/runtime~main.019f9177.js"
+  },
+  {
+    "revision": "3bbe57fb72e7ffdf3855",
+    "url": "/itsm/static/js/main.3bbe57fb.chunk.js"
+  },
+  {
+    "revision": "c309f43fcaa090aa3b9c",
+    "url": "/itsm/static/js/1.c309f43f.chunk.js"
+  },
+  {
+    "revision": "3bbe57fb72e7ffdf3855",
+    "url": "/itsm/static/css/main.1d3d2d1c.chunk.css"
+  },
+  {
+    "revision": "c309f43fcaa090aa3b9c",
+    "url": "/itsm/static/css/1.27d8496d.chunk.css"
+  },
+  {
+    "revision": "b743cca9c6ccea215db1b916e21b8707",
+    "url": "/itsm/index.html"
+  }
+];
