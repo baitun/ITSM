fdcb55b7f9c1e15591c810d9741920a33b75334d
Yury Savin <savinyurii@gmail.com>
2018-11-22 14:11:56 +0800

    .🎨 Make form horizontal

--- src/ModalForm.js
+++ src/ModalForm.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Form, Modal, Input, Radio, Select } from 'antd';
+import { Form, Modal, Input, Radio, Select, Checkbox, DatePicker } from 'antd';
 
 const ModalForm = Form.create()(
   class extends React.Component {
     render() {
       const { visible, onCancel, onCreate, form } = this.props;
       const { getFieldDecorator } = form;
+      const formItemLayout = {
+        labelCol: { span: 6 },
+        wrapperCol: { span: 18 },
+      };
       return (
         <Modal
           visible={visible}
@@ -15,9 +19,9 @@ const ModalForm = Form.create()(
           onOk={onCreate}
         >
           <Form>
-            <Form.Item label="Locality (City)">
-              {getFieldDecorator('title', { rules: [{ required: true }] })(
-                <Select>
+            <Form.Item {...formItemLayout} label="Locality (City)">
+              {getFieldDecorator('title')(
+                <Select mode="multiple">
                   <Select.Option value="0">Irkutsk</Select.Option>
                   <Select.Option value="1">Rostov-on-Don</Select.Option>
                   <Select.Option value="2">Saransk</Select.Option>
@@ -25,8 +29,8 @@ const ModalForm = Form.create()(
                 </Select>
               )}
             </Form.Item>
-            <Form.Item label="System">
-              {getFieldDecorator('system', { rules: [{ required: true }] })(
+            <Form.Item {...formItemLayout} label="System">
+              {getFieldDecorator('system')(
                 <Select>
                   <Select.Option value="0">BPM</Select.Option>
                   <Select.Option value="1">CRM</Select.Option>
@@ -35,11 +39,22 @@ const ModalForm = Form.create()(
                 </Select>
               )}
             </Form.Item>
-            <Form.Item label="Description">
-              {getFieldDecorator('description')(<Input type="textarea" />)}
+            <Form.Item {...formItemLayout} label="Description">
+              {getFieldDecorator('description')(
+                <Input.TextArea autosize={{ minRows: 2 }} />
+              )}
             </Form.Item>
-            <Form.Item label="BPM Ticket ID">
+            <Form.Item {...formItemLayout} label="BPM Ticket ID">
               {getFieldDecorator('descentryidription')(<Input />)}
+            </Form.Item>
+            <Form.Item {...formItemLayout} label="Is open?">
+              {getFieldDecorator('status')(<Checkbox />)}
+            </Form.Item>
+            <Form.Item {...formItemLayout} label="Date start">
+              {getFieldDecorator('bdate')(<DatePicker />)}
+            </Form.Item>
+            <Form.Item {...formItemLayout} label="Date end">
+              {getFieldDecorator('edate')(<DatePicker />)}
             </Form.Item>
           </Form>
         </Modal>
