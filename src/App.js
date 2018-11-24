007377420e0f9ac253fc565e025375eb80938cca
Yury Savin <savinyurii@gmail.com>
2018-11-25 03:29:48 +0800

    ✨Start simple auth

--- src/App.js
+++ src/App.js
@@ -5,31 +5,61 @@ import EditableTable from './EditableTable';
 import { LocaleProvider, Menu } from 'antd';
 import ru_RU from 'antd/lib/locale-provider/ru_RU';
 import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
+import { hasRole, isAllowed } from './auth';
+
+const user = {
+  roles: ['user'],
+  rights: ['can_view_articles'],
+};
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: user,
+    };
+  }
   render() {
+    const { user } = this.state;
     return (
       <LocaleProvider locale={ru_RU}>
         <BrowserRouter>
           <>
-            <Menu mode="horizontal" theme="dark">
-              <Menu.Item>
-                <Link to="/ITSM">Tickets</Link>
+            <Menu mode="horizontal">
+              <Menu.Item
+                onClick={() =>
+                  this.setState(prevState => ({
+                    user: {
+                      roles: prevState.user.roles.includes('admin')
+                        ? ['user']
+                        : ['admin'],
+                    },
+                  }))
+                }
+              >
+                {user.roles.includes('admin') ? 'Logout' : 'Login'}
               </Menu.Item>
+
               <Menu.Item>
-                <Link to="/ITSM/admin">Admin</Link>
+                <Link to="/">Tickets</Link>
               </Menu.Item>
-              <Menu.Item>
-                <Link to="/ITSM/edit">Edit</Link>
-              </Menu.Item>
+              {hasRole(user, ['admin']) && (
+                <Menu.Item>
+                  <Link to="admin">Admin</Link>
+                </Menu.Item>
+              )}
             </Menu>
+
             <h1>IT Service Management</h1>
 
             <Switch>
               <Redirect exact from="/" to="/ITSM/" />
               <Route exact path="/ITSM" component={TableTickets} />
+              {/* @TODO remove it */}
               <Route exact path="/ITSM/edit" component={EditableTable} />
-              <Route exact path="/ITSM/admin" component={TableTicketsAdmin} />
+              {hasRole(user, ['admin']) && (
+                <Route exact path="/ITSM/admin" component={TableTicketsAdmin} />
+              )}
               <Route component={() => <div>404. Page Not Found</div>} />
             </Switch>
           </>
---
+++ src/auth.js
@@ -0,0 +1,9 @@
+// https://alligator.io/react/simple-authorization/
+
+export const isAuthenticated = user => !!user;
+
+export const isAllowed = (user, rights) =>
+  rights.some(right => user.rights.includes(right));
+
+export const hasRole = (user, roles) =>
+  roles.some(role => user.roles.includes(role));
