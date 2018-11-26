ff505f133e16a69a9d769f218a5ff1ea38e062c6
Yury Savin <savinyurii@gmail.com>
2018-11-26 19:27:18 +0800

    🎨 Use Layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import TableTickets from './TableTickets';
 import TableTicketsAdmin from './TableTicketsAdmin';
 import EditableTable from './EditableTable';
-import { LocaleProvider, Menu } from 'antd';
+import { LocaleProvider, Menu, Layout } from 'antd';
 import ru_RU from 'antd/lib/locale-provider/ru_RU';
 import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom';
 import { hasRole, isAllowed } from './auth';
 
 const user = {
-  roles: ['user'],
+  roles: ['admin'],
   rights: ['can_view_articles'],
 };
 
@@ -24,45 +24,69 @@ class App extends React.Component {
     return (
       <LocaleProvider locale={ru_RU}>
         <BrowserRouter>
-          <>
-            <Menu mode="horizontal">
-              <Menu.Item
-                onClick={() =>
-                  this.setState(prevState => ({
-                    user: {
-                      roles: prevState.user.roles.includes('admin')
-                        ? ['user']
-                        : ['admin'],
-                    },
-                  }))
-                }
+          <Layout>
+            <Layout.Header>
+              <div className="logo">ITSM</div>
+              <Menu
+                mode="horizontal"
+                theme="dark"
+                style={{ lineHeight: '64px' }}
               >
-                {user.roles.includes('admin') ? 'Logout' : 'Login'}
-              </Menu.Item>
-
-              <Menu.Item>
-                <Link to="/">Tickets</Link>
-              </Menu.Item>
-              {hasRole(user, ['admin']) && (
-                <Menu.Item>
-                  <Link to="admin">Admin</Link>
+                <Menu.Item
+                  onClick={() =>
+                    this.setState(prevState => ({
+                      user: {
+                        roles: prevState.user.roles.includes('admin')
+                          ? ['user']
+                          : ['admin'],
+                      },
+                    }))
+                  }
+                >
+                  {user.roles.includes('admin') ? 'Logout' : 'Login'}
                 </Menu.Item>
-              )}
-            </Menu>
 
-            <h1>IT Service Management</h1>
+                <Menu.Item>
+                  <Link to="/">Tickets</Link>
+                </Menu.Item>
+                {hasRole(user, ['admin']) && (
+                  <Menu.Item>
+                    <Link to="admin">Admin</Link>
+                  </Menu.Item>
+                )}
+              </Menu>
+            </Layout.Header>
+            <Layout.Content style={{ padding: '0 50px' }}>
+              <div
+                style={{
+                  background: '#fff',
+                  padding: 24,
+                  minHeight: 'calc(100vh - 64px - 68px)',
+                }}
+              >
+                {/* <h1>IT Service Management</h1> */}
 
-            <Switch>
-              <Redirect exact from="/" to="/ITSM/" />
-              <Route exact path="/ITSM" component={TableTickets} />
-              {/* @TODO remove it */}
-              <Route exact path="/ITSM/edit" component={EditableTable} />
-              {hasRole(user, ['admin']) && (
-                <Route exact path="/ITSM/admin" component={TableTicketsAdmin} />
-              )}
-              <Route component={() => <div>404. Page Not Found</div>} />
-            </Switch>
-          </>
+                <Switch>
+                  <Redirect exact from="/" to="/ITSM/" />
+                  <Route exact path="/ITSM" component={TableTickets} />
+                  {/* @TODO remove it */}
+                  <Route exact path="/ITSM/edit" component={EditableTable} />
+                  {hasRole(user, ['admin']) && (
+                    <Route
+                      exact
+                      path="/ITSM/admin"
+                      component={TableTicketsAdmin}
+                    />
+                  )}
+                  <Route component={() => <div>404. Page Not Found</div>} />
+                </Switch>
+              </div>
+              <Layout.Footer style={{ textAlign: 'center' }}>
+                IT Service Management ©{' '}
+                <a href="https://github.com/baitun/ITSM">baitun</a>
+              </Layout.Footer>
+            </Layout.Content>
+          </Layout>
         </BrowserRouter>
       </LocaleProvider>
     );
